41b9bc61d3fdc2bf3fcda346f72c810247a73eea
Christopher-Slaghuis <60656255+Christopher-Slaghuis@users.noreply.github.com>
2020-10-19 13:09:58 +0200

    Add files via upload

---
+++ CMPG 223 Group Project/MovieBookingProgram/MovieBookingProgram/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieBookingProgram
+{
+    public partial class FrmWelcome : Form
+    {
+        public FrmWelcome()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            LogIn log = new LogIn();
+            log.Show();
+            this.Hide();
+        }
+    }
+}
